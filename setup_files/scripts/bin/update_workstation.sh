a7d829272f18d1c6d5ca082971b5958d80205235
Henry Bennett <henry.bennett@spirent.com>
2026-02-01 14:13:54 -0600

    updates

--- setup_files/scripts/bin/update_workstation.sh
+++ setup_files/scripts/bin/update_workstation.sh
@@ -3,6 +3,7 @@ WORKSTATION_DIR=${HOME}/workstation
 
 # Update dotfiles
 for config in $(ls ${WORKSTATION_DIR}/dotfiles); do
+  echo $config
   SOURCE=${WORKSTATION_DIR}/dotfiles/${config}
   TARGET=${HOME}
   stow -t ${TARGET} -d ${SOURCE} $(ls ${SOURCE})
