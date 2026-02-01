1875689ffa45270ac20c89e7374ecaf2c13af43e
Henry Bennett <henry.bennett@spirent.com>
2026-02-01 13:09:54 -0600

    cleanup

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-mkdir -p ansible
-mkdir -p dotfiles
-
-GIT_SSH_COMMAND="ssh -i keys/workstation" git clone git@github.com:henman95/ansible.workstation.git  ansible/workstation
-GIT_SSH_COMMAND="ssh -i keys/workstation" git clone git@github.com:henman95/dotfiles.git dotfiles/core 
-GIT_SSH_COMMAND="ssh -i keys/workstation" git clone git@github.com:henman95/dotfiles.private.git dotfiles/private
